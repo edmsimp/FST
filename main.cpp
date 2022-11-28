a47604c44845aec6d055535e5d8077e9cd511b32
edmsimp <eduardo.msimplicio@gmail.com>
2022-11-28 12:31:27 -0300

    incomplete pseudocode

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <memory>
+#include <set>
+#include <string>
 #include <vector>
 
-#define FIRST_CHAR ('a');
-#define LAST_CHAR ('z');
+/********* Definitions *********/
+
+#define FIRST_CHAR 'a'
+#define LAST_CHAR 'z'
+#define MAX_WORD_SIZE 50
 
 typedef class stateStruct stateStruct;
 typedef std::shared_ptr<stateStruct> state;
+typedef class dictionaryStruct dictionaryStruct;
+typedef std::shared_ptr<dictionaryStruct> dictionary;
 
-typedef class dictionary dictionary;
+/********* State Struct *********/
 
 class stateStruct {
 public:
-    stateStruct() {
-        this->isFinal = false;
-    }
+    stateStruct() = default;
 
     ~stateStruct() = default;
 
     bool getIsFinal () {
         return this->isFinal;
     }
+
     void setIsFinal (bool f) {
         this->isFinal = f;
     }
 
     state getTransition(char c) {
-        this->transitions.at(c);
+        return this->transitions.at(c);
     }
 
     void setTransition(char c, state s) {
@@ -38,38 +45,75 @@ public:
         this->transitions.clear();
     }
 
+    std::set<std::string> getStrings () {
+        return this->strings;
+    }
+
+    void setStrings (std::set<std::string> s) {
+        this->strings = s;
+    }
+
+    std::string getOutput () {
+        return this->output;
+    }
+
+    void setOutput (std::string s) {
+        this->output = s;
+    }
+
 private:
     std::map<char, state> transitions;
+    std::set<std::string> strings; // null if isFinal = false
+    std::string output; // null if isFinal = true
     bool isFinal;
 };
+
+/********* Auxiliary State Functions *********/
 
 state newState () {
     return std::make_shared<stateStruct>();
 }
 
-state copyState (state s) {
-    state ns = newState();
-    *ns = *s;
-    return ns;
+state getTransition (state s, char c) {
+    return s->getTransition(c);
 }
 
-state clearState (state s) {
-    s->clearTransitions();
-    s->setIsFinal(false);
+void setTransition (state s, char c, state ns) {
+    s->setTransition(c, ns);
 }
 
-class dictionary {
+std::set<std::string> stateOutput (state s) {
+    return s->getStrings();
+}
+
+void setStateOutput (state s, std::set<std::string> so) {
+    s->setStrings(so);
+}
+
+std::string output (state s) {
+    return s->getOutput();
+}
+
+void setOutput (state s, std::string so) {
+    s->setOutput(so);
+}
+
+/********* Dictionary *********/
+
+class dictionaryStruct {
 public:
-    dictionary() = default;
+    dictionaryStruct() = default;
 
-    ~dictionary() = default;
+    ~dictionaryStruct() = default;
 
-    state getTransition (state s, char c) {
-        return s->getTransition(c);
+    state findState (state s) {
+        for(std::vector<state>::iterator it = this->states.begin(); it < this->states.end(); it++)
+            if (*it == s) return *it;
+        return nullptr;
     }
 
-    void setTransition (state s, char c, state ns) {
-        s->setTransition(c, ns);
+    void addState (state s) {
+        this->states.emplace_back(s);
     }
 
 private:
@@ -77,8 +121,100 @@ private:
     state parameterState;
 };
 
+/********* Final Functions *********/
+
+state copyState (state s) {
+    state ns = newState();
+    *ns = *s;
+    return ns;
+}
+
+void clearState (state s) {
+    s->clearTransitions();
+    s->setIsFinal(false);
+}
+
+bool compareState (state s1, state s2) {
+    return s1 == s2;
+}
+
+dictionary newDictionary () {
+    return std::make_shared<dictionaryStruct>();
+}
+
+state member (dictionary d, state s) {
+    return d->findState(s);
+}
+
+void insert (dictionary d, state s) {
+    d->addState(s);
+}
+
+/********* Program *********/
+
+dictionary minimalTransducerStatesDictionary;
+
+state findMinimized (state s) {
+    state r;
+    r = member(minimalTransducerStatesDictionary, s);
+
+    if (r == nullptr) {
+        r = copyState(s);
+        insert(minimalTransducerStatesDictionary, s);
+    }
+
+    return r;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::fstream file;
+
+    std::vector<state> tempStates;
+    tempStates.resize(MAX_WORD_SIZE, nullptr);
+    state initialState;
+    std::string previousWord, currentWord, currentOutput,
+        wordSuffix, commonPrefix;
+    std::string tempString;
+    std::set<std::string> tempSet;
+    int i, j, prefixLengthPlus1;
+    char c;
+
+    minimalTransducerStatesDictionary = newDictionary();
+
+    for (i = 0; i < MAX_WORD_SIZE; ++i) {
+        tempStates[i] = newState();
+    }
+
+    previousWord = "";
+    clearState(tempStates[0]);
+
+    file.open ("dict.txt", std::ios::in);
+    if (file.is_open())
+        while (!file.eof()) {
+            std::getline(file, currentWord);
+            // current output should be defined here, but I don't know what it should be
+            std::cout << currentWord << std::endl;
+
+            i = 1;
+            while (i < currentWord.length() and i < previousWord.length() and currentWord[i] == previousWord[i])
+                i++;
+            prefixLengthPlus1 = i;
+
+            for (i = prefixLengthPlus1; i < currentWord.length(); i++) {
+                clearState(tempStates[i]);
+                setTransition(tempStates[i-1], currentWord[i], tempStates[i]);
+            }
+
+            if (currentWord != previousWord) {
+                tempStates[currentWord.length()]->setIsFinal(true);
+                setOutput(tempStates[currentWord.length()], {""});
+            }
+
+            for (j = 1; j < prefixLengthPlus1 - 1; j++) {
+                // TODO: end pseudocode
+            }
+        }
+
+    file.close();
     return 0;
 }
